77f726ab76d55a81d1893f65eef8b405d44cfb49
Towaf H <144939977+TowafH@users.noreply.github.com>
2024-03-12 18:00:31 +0000

    Initial commit

---
+++ script.js
@@ -0,0 +1,15 @@
+function myFunction(){
+  document.getElementById("greet").style.color = "yellow";
+}
+function night(){
+  document.body.style.backgroundColor="darkblue";
+}
+function day(){
+  document.body.style.backgroundColor="lightskyblue";
+}
+function changecolor(){
+  document.getElementById("para").style.color= "darkred";
+}
+function changefont(){
+  document.getElementById("para").style.fontFamily="Comic Sans MS";
+}
